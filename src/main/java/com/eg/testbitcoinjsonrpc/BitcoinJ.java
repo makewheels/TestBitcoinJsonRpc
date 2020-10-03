b0995ac31d2f06792952f691cde85e8268446000
makewheels <makewheels>
2020-10-03 11:16:31 +0800

    BitcoinJ

---
+++ src/main/java/com/eg/testbitcoinjsonrpc/BitcoinJ.java
@@ -0,0 +1,34 @@
+package com.eg.testbitcoinjsonrpc;
+
+import org.bitcoinj.core.ECKey;
+import org.bitcoinj.core.NetworkParameters;
+import org.bitcoinj.kits.WalletAppKit;
+import org.bitcoinj.params.RegTestParams;
+import org.bitcoinj.params.TestNet3Params;
+
+import java.io.File;
+
+public class BitcoinJ {
+    public static void main(String[] args) {
+        NetworkParameters params = TestNet3Params.get();
+        String filePrefix = "forwarding-service-testnet";
+
+        WalletAppKit kit = new WalletAppKit(params, new File("."), filePrefix) {
+            @Override
+            protected void onSetupCompleted() {
+                // This is called in a background thread after startAndWait is called, as setting up various objects
+                // can do disk and network IO that may cause UI jank/stuttering in wallet apps if it were to be done
+                // on the main thread.
+                if (wallet().getKeyChainGroupSize() < 1)
+                    wallet().importKey(new ECKey());
+            }
+        };
+
+        if (params == RegTestParams.get()) {
+            // Regression test mode is designed for testing and development only, so there's no public network for it.
+            // If you pick this mode, you're expected to be running a local "bitcoind -regtest" instance.
+            kit.connectToLocalHost();
+        }
+
+    }
+}
